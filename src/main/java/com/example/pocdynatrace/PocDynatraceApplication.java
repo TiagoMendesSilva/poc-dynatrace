e9aa96f73d384d1e24fb4e0e3d65716f7cddb34a
tiago <tiagoms@ciandt.com>
2022-02-10 11:11:48 -0300

    Backend project created

---
+++ src/main/java/com/example/pocdynatrace/PocDynatraceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pocdynatrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDynatraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocDynatraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pocdynatrace/PocDynatraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocdynatrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocDynatraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
